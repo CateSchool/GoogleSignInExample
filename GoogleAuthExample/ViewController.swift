b48cea365759129d33de3941ae3b4265c63edb67
John MacFarlane <john_macfarlane@cate.org>
2019-04-12 10:13:03 -0700

    A simple example of Google Auth (both login, automatic login, and signout). Need to:  get an OAuth client ID (https://developers.google.com/identity/sign-in/ios/start-integrating)  Add a URL scheme to your project  Update GIDSignIn.sharedInstance().clientID = "YOUR_CLIENT_ID" with proper OAuth ID.

---
+++ GoogleAuthExample/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  GoogleAuthExample
+//
+//  Created by John MacFarlane on 4/11/19.
+//  Copyright © 2019 Cate School. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+
+class ViewController: UIViewController, GIDSignInUIDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Mark myself as the delegte
+        GIDSignIn.sharedInstance().uiDelegate = self
+        
+        // To automatically sign in the user.
+        GIDSignIn.sharedInstance().signInSilently()
+        
+        // TODO(developer) Configure the sign-in button look/feel
+        
+    }
+
+    @IBAction func signOutOfGoogle(_ sender: UIButton) {
+        GIDSignIn.sharedInstance().signOut()
+    }
+    
+}
+
